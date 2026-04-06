1126eb637f2d7d65e680dc751fb9e9e9a73cf048
RAJAPPA ADABALA <109161339+rajappa-adabala@users.noreply.github.com>
2026-04-06 19:28:36 +0530

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int mxprofit=0,curr=0;
+        for(int i=1;i<prices.length;i++){
+            curr+=prices[i]-prices[i-1];
+            if(curr<0) curr=0;
+            mxprofit=Math.max(mxprofit,curr);
+
+        }return mxprofit;
+
+        
+    }
+}
